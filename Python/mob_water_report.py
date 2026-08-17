2d9f820528810c03e6954e14237a14c734eb7163
Jared Taylor <jaredtaylor.99@gmail.com>
2026-08-17 22:32:13 +1200

    Correct the pages the last two features made wrong

--- Python/mob_water_report.py
+++ Python/mob_water_report.py
@@ -38,6 +38,7 @@ SPECTRUM_ROOT = '/MobWater/Spectra'
 MASTERS = [
     'M_MobWater',
     'M_MobWaterUnderwater',
+    'M_MobWaterExclusionField',
     'M_MobWaterRippleStep',
     'M_MobWaterRippleCopy',
     'M_MobWaterRippleStamp',
